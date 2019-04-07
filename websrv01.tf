52023d6fe4d042321a34f9c10bcb45227cc9c432
OiSiS <oisis@o2.pl>
2019-04-07 21:41:26 +0200

    Create first webserver with Nginx

---
+++ websrv01.tf
@@ -0,0 +1,20 @@
+resource "digitalocean_droplet" "websrv01" {
+  image              = "ubuntu-18-10-x64"
+  name               = "websrv01"
+  region             = "fra1"
+  size               = "512mb"
+  ipv6               = false
+  private_networking = true
+  user_data          = "${file("config/webuserdata.sh")}"
+
+  ssh_keys = [
+    "${var.ssh_fingerprint}",
+  ]
+
+  connection {
+    user        = "root"
+    type        = "ssh"
+    private_key = "${file(var.pvt_key)}"
+    timeout     = "2m"
+  }
+}
